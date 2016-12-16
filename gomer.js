1862f6968fd9083adf9e4d81e59d0dad1366568e
David Diaz <ddiaz914@gmail.com>
2016-12-16 03:05:39 -0600

    Add comments for the new variables and methods.

--- gomer.js
+++ gomer.js
@@ -1,29 +1,39 @@
+// Grab each node for easy selection
 const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
 const startButton = document.querySelector('.startButton');
 const stopButton = document.querySelector('.stopButton');
 const breakButton = document.querySelector('.breakButton');
+
+// Set intervalId globally so that setInterval can be easily cleared
 let intervalId = null;
 
+// Update the html timer according to the time captured to overcome javascript throttling in inactive tabs
 function remainingTime(eTime){
+  // Capture the new current time each time the method is called
   const currentTime = Date.parse(new Date());
 
+  // Checks to see if the minutes are below 10 so that it can add the inital zero in the front
   if(Math.floor( ((eTime-currentTime)/1000/60) % 60 ) >= 10){
     minutes.textContent = `${Math.floor( ((eTime-currentTime)/1000/60) % 60 )}`;
   } else {
     minutes.textContent = `0${Math.floor( ((eTime-currentTime)/1000/60) % 60 )}`;
   }
 
+  // Checks to see if the seconds are below 10 so that it can add the inital zero in the front
   if(Math.floor( ((eTime-currentTime)/1000) % 60 ) >= 10){
     seconds.textContent = `${Math.floor( ((eTime-currentTime)/1000) % 60 )}`;
   } else {
     seconds.textContent = `0${Math.floor( ((eTime-currentTime)/1000) % 60 )}`;
   }
 
+  // Checks if the timer has reached zero, and if it has stop the setInterval
   checkIfTimerIsZero();
 }
 
+// Check if the timer has reached zero
 function checkIfTimerIsZero(){
+  // If the timer has reached zero, play the gong noise
   if((Number(minutes.textContent) + Number(seconds.textContent)) === 0){
     var noise = new Audio("gong.mp3");
     noise.play();
@@ -31,25 +41,39 @@ function checkIfTimerIsZero(){
   }
 }
 
+// Check to see if the start button has been pushed
 startButton.addEventListener('click', function(event){
+  // Clear any setInterval calls that are being made
   clearInterval(intervalId);
+  // Change the minutes and seconds to 25
   minutes.textContent = "25";
   seconds.textContent = "00";
+  // Save the time of when the timer starts
   const startTime = Date.parse(new Date());
+  // Save the time of when it should end
   const endTime = Date.parse(new Date(startTime + (25*60*1000)));
+  // Start the countdown
   intervalId = setInterval(remainingTime, 1000, endTime);
 });
 
+// Check to see if the break button has been pushed
 breakButton.addEventListener('click', function(event){
+  // Clear any setInterval calls that are being made
   clearInterval(intervalId);
+  // Change the minutes and seconds to 5
   minutes.textContent = "05";
   seconds.textContent = "00";
+  // Save the time of when the timer starts
   const startTime = Date.parse(new Date());
+  // Save the time of when it should end
   const endTime = Date.parse(new Date(startTime + (5*60*1000)));
+  // Start the countdown
   intervalId = setInterval(remainingTime, 1000, endTime);
 });
 
+// Check to see if the stop button has been pushed
 stopButton.addEventListener('click', function(event){
+  // Clear any setInterval calls that are being made
   clearInterval(intervalId);
 });
 
