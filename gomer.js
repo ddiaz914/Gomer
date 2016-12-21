743d348bffe160f26bfa91f3c4c137f1fc8c87c8
David Diaz <ddiaz914@gmail.com>
2016-12-21 01:48:05 -0600

    Add comments to the createNewNote function as well as move it to the bottom of the file closer to the function that calls it.

--- gomer.js
+++ gomer.js
@@ -90,22 +90,89 @@ const notesList = document.querySelector('.notesList');
 const addNoteButton = document.querySelector('.addNoteButton');
 const submitNoteButton = document.querySelector('.submitNoteButton');
 
+// Automatically hide the Notes list as soon as the page loads
+notesList.style.display = 'none';
+
+// Functionality for 'Add a Note' button
+addNoteButton.addEventListener('click', function(event){
+  // Show the form for adding a note
+  noteForm.style.visibility = 'visible';
+  // Blur everything behind the form
+  notes.classList.add('blur');
+  timer.classList.add('blur');
+  // Put the form into focus
+  noteFormInput.focus();
+});
+
+// If the form is no longer in focus, the form disappears and the blur effect is removed
+noteForm.addEventListener('focusout', function(event){
+  // Hide the form
+  this.style.visibility = 'hidden';
+  // Unblur everything behind the form
+  notes.classList.remove('blur');
+  timer.classList.remove('blur');
+  // Reset the form
+  noteForm.reset();
+});
+
+// When the enter key is pushed for a note to be submitted, the blur effect is removed and the new note is appended to the notes list
+submitNoteButton.addEventListener('click', function(event){
+  event.preventDefault();
+
+  // The reason for this if statement, is because I only want these methods to fire when the list was originally 0, and not everytime the submit button is clicked
+  if(notesList.children.length === 0){
+    notesList.style.display = 'inline';
+  }
+
+  // Append the value from the form into a new list item on the page
+  createNewNote(document.querySelector('.noteFormInput').value);
+
+  // Remove the blur class
+  notes.classList.remove('blur');
+  timer.classList.remove('blur');
+
+  // Reset the form, just in case there was information entered
+  noteForm.reset();
+
+  // Hide the form
+  noteForm.style.visibility = 'hidden';
+});
+
 // This function creates a new note with event listeners attached to be inserted into the DOM
 function createNewNote(input){
+
+  // Create an empty li element
   let li = document.createElement('li');
+  // Give it a class of todo
   li.classList.add('todo');
+
+  // Create and empty span element
   let noteSpan = document.createElement('span');
+  // Give it a class of todoText
   noteSpan.classList.add('todoText');
-  let checkmarkSpan = document.createElement('span');
-  checkmarkSpan.classList.add('complete');
-  let deleteSpan = document.createElement('span');
-  deleteSpan.classList.add('delete');
+  // Creates a text node for the note
   let noteText = document.createTextNode(input);
-  let deleteText = document.createTextNode('X');
+
+  // Create and empty span element
+  let checkmarkSpan = document.createElement('span');
+  // Give it a class of complete
+  checkmarkSpan.classList.add('complete');
+  // Creates a text node for the checkmark button
   let checkmarkText = document.createTextNode('√');
+
+  // Create and empty span element
+  let deleteSpan = document.createElement('span');
+  // Give it a class of delete
+  deleteSpan.classList.add('delete');
+  // Creates a text node for the delete button
+  let deleteText = document.createTextNode('X');
+
+  // Append the text to the parent nodes
   noteSpan.appendChild(noteText);
   checkmarkSpan.appendChild(checkmarkText);
   deleteSpan.appendChild(deleteText);
+
+  // Append the spans to their parent nodes
   li.appendChild(noteSpan);
   li.appendChild(checkmarkSpan);
   li.appendChild(deleteSpan);
@@ -142,50 +209,3 @@ function createNewNote(input){
     }
   });
 }
-
-// Automatically hide the Notes list as soon as the page loads
-notesList.style.display = 'none';
-
-// Functionality for 'Add a Note' button
-addNoteButton.addEventListener('click', function(event){
-  // Show the form for adding a note
-  noteForm.style.visibility = 'visible';
-  // Blur everything behind the form
-  notes.classList.add('blur');
-  timer.classList.add('blur');
-  // Put the form into focus
-  noteFormInput.focus();
-});
-
-// If the form is no longer in focus, the form disappears and the blur effect is removed
-noteForm.addEventListener('focusout', function(event){
-  // Hide the form
-  this.style.visibility = 'hidden';
-  // Unblur everything behind the form
-  notes.classList.remove('blur');
-  timer.classList.remove('blur');
-  // Reset the form
-  noteForm.reset();
-});
-
-submitNoteButton.addEventListener('click', function(event){
-  event.preventDefault();
-
-  // The reason for this if statement, is because I only want these methods to fire when the list was originally 0, and not everytime the submit button is clicked
-  if(notesList.children.length === 0){
-    notesList.style.display = 'inline';
-  }
-
-  // Append the value from the form into a new list item on the page
-  createNewNote(document.querySelector('.noteFormInput').value);
-
-  // Remove the blur class
-  notes.classList.remove('blur');
-  timer.classList.remove('blur');
-
-  // Reset the form, just in case there was information entered
-  noteForm.reset();
-
-  // Hide the form
-  noteForm.style.visibility = 'hidden';
-});
